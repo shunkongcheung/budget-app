2d68b6db4dc0d3fcb9488097a97d4cf6d2a0b5e4
Shun Cheung <shunkongcheung@gmail.com>
2020-10-18 18:15:17 -0400

    feat(api/budgets): init

--- src/pages/api/budgets/index.ts
+++ src/pages/api/budgets/index.ts
@@ -1,16 +1,37 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from "next";
 
-import { isUserLoggedIn, getValidatePayload } from "../../../utils";
-
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {
+  getFirebaseAdmin,
+  getLoggedInUser,
+  getValidatePayload,
+} from "../../../utils";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  await isUserLoggedIn(req.cookies.token);
+  const user = await getLoggedInUser(req.cookies.token);
+  const db = getFirebaseAdmin().firestore();
+  const docRef = db.collection("budgets");
 
-  const data = await getValidatePayload(req.body);
+  if (req.method === "GET") {
+    res.statusCode = 200;
+    const snapshot = await docRef.where("createdBy", "==", user.uid).get();
+    if (snapshot.empty) {
+      res.json([]);
+    } else {
+      const data = [];
+      snapshot.forEach((doc) => {
+        const item = doc.data();
+        data.push({ ...item, id: doc.id, date: item.date.toString() });
+      });
+      res.json(data);
+    }
+  }
+  if (req.method === "POST") {
+    const data = await getValidatePayload(req.body);
 
-  console.log(data);
+    await docRef.doc().set({ ...data, createdBy: user.uid });
 
-  res.statusCode = 200;
-  res.json(data);
+    res.statusCode = 200;
+    res.json(data);
+  }
 };
