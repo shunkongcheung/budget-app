e5dd3a930d194aae2f50aa23c0e9347734c36367
Shun Cheung <shunkongcheung@gmail.com>
2020-10-18 18:17:02 -0400

    feat(budgets): fetch cookie simplify

--- src/pages/budgets/index.tsx
+++ src/pages/budgets/index.tsx
@@ -1,5 +1,8 @@
-import Budgets from "../../containers/Budgets";
+import { GetServerSidePropsContext } from "next";
+import axios from "axios";
 import moment, { Moment } from "moment";
+
+import Budgets from "../../containers/Budgets";
 
 type Category = "B&F" | "Supplies" | "Bill";
 
@@ -15,58 +18,17 @@ interface BudgetsPageProps {
   budgets: Array<BudgetItem>;
 }
 
-// should be remove when complet
-const getRandomDate = (start: Date) => {
-  const end = new Date();
-  return new Date(
-    start.getTime() + Math.random() * (end.getTime() - start.getTime())
-  );
-};
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const { host, cookie } = ctx.req.headers;
 
-export const getServerSideProps = async () => {
-  // TODO: complete this section
-  const start0 = new Date();
-  start0.setDate(start0.getDate() - 10);
-
-  const start1 = new Date();
-  start1.setHours(start1.getHours() - 3);
-
-  const start2 = new Date();
-  start2.setMinutes(start2.getMinutes() - 10);
+  const data = await new Promise((resolve) => {
+    axios
+      .get(`http://${host}/api/budgets`, { headers: { cookie } })
+      .then((res) => resolve(res.data));
+  });
 
   return {
-    props: {
-      budgets: [
-        {
-          id: "item1",
-          title: "Eat with my babe",
-          category: "B&F",
-          date: getRandomDate(start0).toISOString(),
-          amount: 20.5,
-        },
-        {
-          id: "item2",
-          title: "Purchased some medicine",
-          category: "Supplies",
-          date: getRandomDate(start1).toISOString(),
-          amount: 45.2,
-        },
-        {
-          id: "item3",
-          title: "Mobile Phone",
-          category: "Bill",
-          date: getRandomDate(start2).toISOString(),
-          amount: 30.5,
-        },
-        {
-          id: "item4",
-          title: "Lunch",
-          category: "B&F",
-          date: getRandomDate(new Date()).toISOString(),
-          amount: 15,
-        },
-      ],
-    },
+    props: { budgets: data as BudgetItem },
   };
 };
 
