ee9f97ff4902ac8c9a01433d9eef04b0b1973336
Shun Cheung <shunkongcheung@gmail.com>
2020-10-18 13:55:57 -0400

    Feat(Layout): add containers

--- src/containers/Layout/index.tsx
+++ src/containers/Layout/index.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement, useCallback, useState } from "react";
+import Head from "next/head";
 
 import Footer from "./Footer";
 import Header from "./Header";
@@ -20,6 +21,19 @@ function App({ children }: AppProps) {
 
   return (
     <>
+      <Head>
+        <title>Budget App</title>
+        <meta
+          name="description"
+          content="An example of using nextjs, reactjs, firebase, firestore, authentication to build a budget app"
+        />
+        <meta
+          name="keywords"
+          content="nextjs, reactjs, firebase, firestore, authentication"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <Header handleLogout={handleLogout} />
       <div className={classNames.container}>{children}</div>
       <div className={classNames.quote}>
