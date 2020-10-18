fa198d07f194bab8c9094c2ad20fe55790096a08
Shun Cheung <shunkongcheung@gmail.com>
2020-10-18 18:31:18 -0400

    Feat(api/budgets/id): finish up

--- src/pages/api/budgets/[id].ts
+++ src/pages/api/budgets/[id].ts
@@ -1,17 +1,50 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-import { isUserLoggedIn, getValidatePayload } from "../../../utils";
+import {
+  getFirebaseAdmin,
+  getLoggedInUser,
+  getValidatePayload,
+} from "../../../utils";
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  await isUserLoggedIn(req.cookies.token);
+  const { id } = req.query;
+  const user = await getLoggedInUser(req.cookies.token);
 
+  const db = getFirebaseAdmin().firestore();
+  const existingData = (
+    await db
+      .collection("budgets")
+      .doc(id as string)
+      .get()
+  ).data();
+
+  if (existingData.createdBy !== user.uid) {
+    res.statusCode = 404;
+    res.json({ message: "Data not found" });
+    return;
+  }
+
+  if (req.method === "GET") {
+    res.statusCode = 200;
+    res.json(existingData);
+  }
   if (req.method === "PUT") {
     const data = await getValidatePayload(req.body);
+    await db
+      .collection("budgets")
+      .doc(id as string)
+      .set({ ...data, createdBy: user.uid });
+
     res.statusCode = 200;
     res.json(data);
   } else if (req.method === "DELETE") {
+    await db
+      .collection("budgets")
+      .doc(id as string)
+      .delete();
+
     res.statusCode = 204;
     res.send("");
   } else {
