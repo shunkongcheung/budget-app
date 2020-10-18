19be1e8d4cf2f14ddfe2cdf26b141879f286cb95
Shun Cheung <shunkongcheung@gmail.com>
2020-10-18 19:21:50 -0400

    feat(index): fetch data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,43 @@
+import { GetServerSidePropsContext } from "next";
+import axios from "axios";
+
 import Statistics from "../containers/Statistics";
+
+export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
+  const { host, cookie } = ctx.req.headers;
+
+  try {
+    const [categories, days] = await Promise.all([
+      new Promise((resolve) => {
+        axios
+          .get(`http://${host}/api/statistics/categories`, {
+            headers: { cookie },
+          })
+          .then((res) => resolve(res.data));
+      }),
+      new Promise((resolve) => {
+        axios
+          .get(`http://${host}/api/statistics/days`, {
+            headers: { cookie },
+          })
+          .then((res) => resolve(res.data));
+      }),
+    ]);
+
+    return {
+      props: {
+        categories,
+      },
+    };
+  } catch (err) {
+    const { url } = ctx.req;
+    ctx.res.setHeader("location", `/login?goTo=${url}`);
+    ctx.res.statusCode = 302;
+    ctx.res.end();
+
+    return { props: {} };
+  }
+};
 
 export default function HomePage() {
   return <Statistics />;
