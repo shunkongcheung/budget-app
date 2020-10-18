ddfe65981689a42a719ee759cb00ba265324c2dd
Shun Cheung <shunkongcheung@gmail.com>
2020-10-18 19:20:44 -0400

    feat(getLoggedInUser): remove console

--- src/utils/getLoggedInUser.ts
+++ src/utils/getLoggedInUser.ts
@@ -1,7 +1,6 @@
 import getFirebaseAdmin from "./getFirebaseAdmin";
 const getLoggedInUser = (token: string) => {
   const admin = getFirebaseAdmin();
-  console.log("hey here...", token);
   return admin.auth().verifyIdToken(token);
 };
 
