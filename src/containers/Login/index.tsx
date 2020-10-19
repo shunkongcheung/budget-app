a33f82047af4730ad57d6b7f6e05e96d60b9f107
Shun Cheung <shunkongcheung@gmail.com>
2020-10-19 13:54:32 -0400

    feat(Login): stylings

--- src/containers/Login/index.tsx
+++ src/containers/Login/index.tsx
@@ -27,15 +27,27 @@ const Login: React.FC<LoginProps> = () => {
   }, [storeToken, router]);
 
   return (
-    <>
-      <h1>Login with Social Media Account</h1>
-      <div>
-        <button className={classNames.loginBtn} onClick={handleLogin}>
-          <img src="/google.jpg" alt="google icon" height="50" />
-          <span className={classNames.loginTxt}>LOGIN WITH GOOGLE</span>
-        </button>
+    <div className={classNames.container}>
+      <img
+        className={classNames.background}
+        alt="desktop-background"
+        src="/login-background.jpg"
+      />
+      <div className={classNames.content}>
+        <div className={classNames.card}>
+          <h1 className={classNames.title}>畢積</h1>
+          <p className={classNames.description}>
+            Welcome to Budget App. <br />
+            <br />
+            Getting started with your Google Account.
+          </p>
+          <button className={classNames.loginBtn} onClick={handleLogin}>
+            <img src="/google.jpg" alt="google icon" height="50" />
+            <span className={classNames.loginTxt}>LOGIN</span>
+          </button>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
