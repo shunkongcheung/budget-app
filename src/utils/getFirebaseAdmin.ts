cdd0902080ec5e598036c691fa03f63f3d6397d0
Shun Cheung <shunkongcheung@gmail.com>
2020-10-19 11:04:03 -0400

    feat(getFirebaseAdmin): handle local

--- src/utils/getFirebaseAdmin.ts
+++ src/utils/getFirebaseAdmin.ts
@@ -1,14 +1,20 @@
 import admin from "firebase-admin";
 
+const getPrivateKey = () => {
+  try {
+    return JSON.parse(process.env.FIREBASE_PRIVATE_KEY);
+  } catch (err) {
+    return process.env.FIREBASE_PRIVATE_KEY;
+  }
+};
+
 const getFirebaseAdmin = () => {
   try {
     const serviceAccount = {
       type: "service_account",
       project_id: "budget-app-be403",
       private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-      // private_key: process.env.FIREBASE_PRIVATE_KEY,
-      // private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-      private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
+      private_key: getPrivateKey(),
       client_email: process.env.FIREBASE_CLIENT_EMAIL,
       client_id: process.env.FIREBASE_CLIENT_ID,
       auth_uri: "https://accounts.google.com/o/oauth2/auth",
