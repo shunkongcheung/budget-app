2c741a9332e1c669c11843e59c3ce21db2e2044c
Shun Cheung <shunkongcheung@gmail.com>
2020-10-19 10:13:47 -0400

    feat(budgets): sort

--- src/pages/api/budgets/index.ts
+++ src/pages/api/budgets/index.ts
@@ -1,6 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from "next";
-import moment from "moment";
 
 import {
   getFirebaseAdmin,
@@ -9,14 +8,16 @@ import {
 } from "../../../utils";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log("at api budgets...", req.cookies);
   const user = await getLoggedInUser(req.cookies.token);
   const db = getFirebaseAdmin().firestore();
   const docRef = db.collection("budgets");
 
   if (req.method === "GET") {
     res.statusCode = 200;
-    const snapshot = await docRef.where("createdBy", "==", user.uid).get();
+    const snapshot = await docRef
+      .where("createdBy", "==", user.uid)
+      .orderBy("date", "desc")
+      .get();
     if (snapshot.empty) {
       res.json([]);
     } else {
@@ -24,13 +25,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       snapshot.forEach((doc) => {
         const item = doc.data();
         data.push({ ...item, id: doc.id, date: item.date.toDate().toString() });
-      });
-
-      data.sort((a, b) => {
-        const [aDate, bDate] = [moment(a.date), moment(b.date)];
-        if (aDate > bDate) return 1;
-        if (aDate < bDate) return -1;
-        if (aDate === bDate) return 0;
       });
       res.json(data);
     }
